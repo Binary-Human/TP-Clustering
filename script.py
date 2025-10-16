3d0f6824468136da88540a071bb5e7d2ff4aa40d
Binary-Human <mll.sara.bakri@gmail.com>
2025-10-16 09:16:05 +0200

    initialize for work

---
+++ script.py
@@ -0,0 +1,12 @@
+# Davies Bouldin
+# Slihouette 
+# Calinkski
+
+# automatisation des solutions + Récuperation des paramètres optimaux
+
+
+
+from sklearn.metrics import (silhouette_score, calinski_harabasz_score, davies_bouldin_score)
+
+# Load la donnees
+# La visualiser
